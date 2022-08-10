98daf1fa649247895e0d8d72b21048cd3f0a4fc1
burcarz <zacharyburcar@gmail.com>
2022-08-10 10:33:40 -0400

    config change

--- config/connection.js
+++ config/connection.js
@@ -9,15 +9,30 @@ const db_host = process.env.DB_HOST
 // local db instantiation
 console.log('instance created')
 const sequelize = new Sequelize(
-    process.env.DB_NAME, 
+    process.env.DB_NAME,
     process.env.DB_USER, 
     process.env.DB_PW, 
 {
     host: db_host,
     dialect: 'postgres',
-    port: 22
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 1,
+    },
+
+    dialectOptions: {
+        connectTimeout: 20000, // default is 10s which causes occasional ETIMEDOUT errors (see https://stackoverflow.com/a/52465919/491553)
+      },
 });
 
+sequelize.authenticate().then(function() {
+    console.log('connection successful');
+}, function(err) {
+    console.log(err)
+})
+ 
 // const sequelize = new Sequelize(
 //     'customer_data_db', 
 //     'root', 
